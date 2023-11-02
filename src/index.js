d294e8441fdf38463a2a889f92927c535bfd864d
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-11-02 18:12:47 +0000

    Added some HTML, working on functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import _ from 'lodash';
+import { format } from 'date-fns';
 
-https://api.weatherapi.com/v1/forecast.json?key=5a27d69e27f140f58e7141821230211&q=felixstowe&days=3
+function component() {
+    const element = document.createElement('div');
+    element.className = "window-div"
+
+    const displayWeather = addDiv('displayWeather', element);
+    const tempBox = addDiv('tempBox', displayWeather);
+    const locationBox = addDiv('locationBox', displayWeather);
+    const dayBox = addDiv('dayBox', displayWeather);
+
+    const searchDiv = addDiv('searchDiv', element);
+    const searchBox = document.createElement("input");
+    searchBox.className = "searchBox";
+    searchDiv.appendChild(searchBox);
+
+
+
+    return element;
+}
+
+document.body.appendChild(component());
+
+function addDiv(className, appendThis){
+    const sectionName = document.createElement('div');
+    sectionName.className = className;
+    appendThis.appendChild(sectionName);
+
+    return sectionName;
+}
+
+
+function getLocation(location){
+    lowercaseLocation = location.toLowerCase();
+    return "https://api.weatherapi.com/v1/forecast.json?key=5a27d69e27f140f58e7141821230211&q=" + lowercaseLocation + "&days=3";
+}
 
 fetch('https://api.weatherapi.com/v1/forecast.json?key=5a27d69e27f140f58e7141821230211&q=felixstowe&days=3')
 .then(function(response){
@@ -11,7 +45,9 @@ fetch('https://api.weatherapi.com/v1/forecast.json?key=5a27d69e27f140f58e7141821
     const forecastdayArray =  forecast.forecastday;
 
     forecastdayArray.forEach(function(day){
-        console.log("Date: " + day.date);
+        const date = new Date(day.date);
+        const formattedDate = format(date, 'eeee');
+        console.log(formattedDate);
         console.log("Moon Phase: " + day.astro.moon_phase);
         console.log("Min Temperature: " + day.day.mintemp_c + "°C");
         console.log("Max Temperature: " + day.day.maxtemp_c + "°C");
@@ -25,7 +61,7 @@ fetch('https://api.weatherapi.com/v1/forecast.json?key=5a27d69e27f140f58e7141821
             console.log("Chance of snow: " + day.day.daily_chance_of_snow);
         }
         if(day.day.maxwind_mph > 30){
-            console.log("Heavy winds possible today, Max windspeed: " + day.day.maxwind_mph);
+            console.log("High winds possible today, Max windspeed: " + day.day.maxwind_mph);
         }
         console.log("");
     });
