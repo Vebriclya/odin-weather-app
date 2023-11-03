99122689430ebbe32cf8004c20b67512d7a8baf1
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-11-03 16:25:10 +0000

    added code to populate the weather sections

--- src/index.js
+++ src/index.js
@@ -6,22 +6,79 @@ function component() {
     const windowDiv = document.createElement('div');
     windowDiv.className = "window-div";
 
-    addDiv("header", windowDiv);
+    const header = addDiv("header", windowDiv);
+    const locationDiv = addDiv("location-div", header);
+    const searchBox = document.createElement("input");
+    header.appendChild(searchBox);
 
     for (let index = 1; index < 4; index++) {
         generateWeatherSection(("day-" + index), windowDiv);
     }
 
 
-    loadWeather();
+    document.addEventListener("DOMContentLoaded", function () {
+        fetch('https://api.weatherapi.com/v1/forecast.json?key=5a27d69e27f140f58e7141821230211&q=felixstowe&days=3')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                // SETTING THE HEADER TEXT
+                const forecast = response.forecast;
+                const location = response.location.name + ", " + response.location.country;
+                const forecastDayArray = forecast.forecastday;
+                locationDiv.innerText = location;
+                
+                let index = 0;
 
+                forecastDayArray.forEach(function (day, index) {
+                    const date = new Date(day.date);
+                    let formattedDate = format(date, 'eeee');
+                    let moonPhase = day.astro.moon_phase;
+                    let averageTemp = day.day.avgtemp_c;
+                    let condition = day.day.condition.text;
+                    let humidity = day.day.avghumidity;
+                    let rainChance = day.day.daily_chance_of_rain;
+                    let snowChance = day.day.daily_chance_of_snow;
+                    let windspeed = day.day.maxwind_mph;
+
+                    let dayDiv = document.getElementById(`day-${index + 1}`);
+                    console.log(dayDiv);
+                    dayDiv.querySelector(".day").textContent = formattedDate;
+                    dayDiv.querySelector(".moon-phase").textContent = moonPhase;
+                    dayDiv.querySelector(".temp-text").textContent = averageTemp;
+                    dayDiv.querySelector(".condition").textContent = condition;
+                    dayDiv.querySelector(".humidity").textContent = `Humidity: ${humidity}%`;
+                    if(rainChance > 50){
+                        dayDiv.querySelector(".rain").textContent = rainChance + "% chance of rain";
+                    }
+                    if(snowChance > 50){
+                        dayDiv.querySelector(".snow").textContent = snowChance + "% chance of snow";
+                    }
+                    if(windspeed > 30){
+                        dayDiv.querySelector(".winds").textContent = "High winds of " + windspeed + "mph possible";
+                    }
+                }
+                );
+
+
+                for (let index = 1; index < 4; index++) {
+                    let weatherBox = document.getElementById(("day-" + index));
+
+
+
+                }
+            })
+    });
+
+
+    //loadWeather();
 
     return windowDiv;
 }
 
 document.body.appendChild(component());
 
-function addDiv(className, appendThis){
+function addDiv(className, appendThis) {
     const sectionName = document.createElement('div');
     sectionName.className = className;
     appendThis.appendChild(sectionName);
@@ -29,50 +86,31 @@ function addDiv(className, appendThis){
     return sectionName;
 }
 
-function generateWeatherSection(id, appendThis){
+function generateWeatherSection(id, appendThis) {
     let weatherSection = addDiv("weather-section", appendThis);
     weatherSection.setAttribute('id', id);
+
+    let dayAndMoonphase = addDiv("day-and-moonphase", weatherSection);
+    addDiv("day", dayAndMoonphase);
+    addDiv("moon-phase", dayAndMoonphase);
+
+    let weatherBox = addDiv("weather-box", weatherSection);
+    let temperature = addDiv("temperature", weatherBox);
+    addDiv("temp-text", temperature);
+    addDiv("temp-format", temperature);
+    addDiv("vl", weatherBox);
+    let details = addDiv("details", weatherBox);
+    addDiv("condition", details);
+    addDiv("humidity", details);
+    addDiv("rain", details);
+    addDiv("snow", details);
+    addDiv("winds", details);
 
     appendThis.appendChild(weatherSection);
     return weatherSection;
 }
 
-function getLocation(location){
+function getLocation(location) {
     lowercaseLocation = location.toLowerCase();
     return "https://api.weatherapi.com/v1/forecast.json?key=5a27d69e27f140f58e7141821230211&q=" + lowercaseLocation + "&days=3";
 }
-
-function loadWeather(location = "felixstowe"){
-    fetch('https://api.weatherapi.com/v1/forecast.json?key=5a27d69e27f140f58e7141821230211&q=felixstowe&days=3')
-.then(function(response){
-    return response.json();
-})
-.then(function(response) {
-    const forecast = response.forecast;
-    const forecastdayArray =  forecast.forecastday;
-
-    forecastdayArray.forEach(function(day){
-        const date = new Date(day.date);
-        const formattedDate = format(date, 'eeee');
-        console.log(formattedDate);
-        console.log("Moon Phase: " + day.astro.moon_phase);
-        console.log("Temperature: " + day.day.avgtemp_c + "°C");
-        console.log("Condition: " + day.day.condition.text);
-        console.log("Humidity: " + day.day.avghumidity + "%");
-        if(day.day.daily_chance_of_rain > 50){
-            console.log("Chance of rain: " + day.day.daily_chance_of_rain + "%");
-        }
-        if(day.day.daily_chance_of_snow > 50){
-            console.log("Chance of snow: " + day.day.daily_chance_of_snow);
-        }
-        if(day.day.maxwind_mph > 30){
-            console.log("High winds possible today, Max windspeed: " + day.day.maxwind_mph + "mph");
-        }
-        console.log("");
-    });
-})
-.catch(function(err){
-    console.error(err);
-});
-}
-
